13468fc1e1fbd8e3954c13e157136b35bfb07e62
Alpha3433 <badassborus@gmail.com>
2025-08-25 02:58:35 +0000

    auto-commit for 5af94621-3c6f-451f-9cdd-1b056fd73d3b

--- components/CustomizationModal.jsx
+++ components/CustomizationModal.jsx
@@ -1482,25 +1482,6 @@ export default CustomizationModal
                           )}
                         </div>
 
-                        {/* Landscape Hat */}
-                        {playerData.equippedHat && (
-                          <div className={`absolute -top-2 left-1/2 transform -translate-x-1/2 ${
-                            playerData.equippedHat === 'crown_gold' ? 
-                              'w-6 h-4 bg-gradient-to-t from-yellow-500 to-yellow-300 rounded-t-lg border border-yellow-600' :
-                            playerData.equippedHat === 'cap_baseball' ? 
-                              'w-8 h-3 bg-gradient-to-r from-red-600 to-red-500 rounded-full' :
-                            playerData.equippedHat === 'helmet_space' ? 
-                              'w-10 h-6 bg-gradient-to-b from-gray-300 to-gray-500 rounded-full border-2 border-blue-400' :
-                              'w-4 h-3 bg-gray-600 rounded'
-                          }`}>
-                            {playerData.equippedHat === 'crown_gold' && (
-                              <div className="flex justify-center">
-                                <div className="w-0.5 h-1.5 bg-yellow-300 rounded-t-full mt-0.5"></div>
-                              </div>
-                            )}
-                          </div>
-                        )}
-
                         {/* Landscape Trail */}
                         {playerData.equippedTrail && (
                           <div className="absolute -right-12 top-1/2 transform -translate-y-1/2 flex space-x-0.5">
