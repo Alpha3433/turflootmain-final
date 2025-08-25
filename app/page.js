07fc00fefa780ed03f90a2409c9ea50a8658dba9
Alpha3433 <badassborus@gmail.com>
2025-08-25 05:17:47 +0000

    auto-commit for 04615ef0-2c4e-4899-a1a5-c6052b30f4cd

--- app/page.js
+++ app/page.js
@@ -1825,8 +1825,6 @@ export default function Home() {
         <div className="h-16"></div>
       </div>
 
-
-
       {/* Welcome Message Popup */}
       {showWelcome && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm">
