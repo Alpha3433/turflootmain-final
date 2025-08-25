8439ebb462c8bfaba708963a8c0662d1bf8b5bd5
Alpha3433 <badassborus@gmail.com>
2025-08-25 02:59:20 +0000

    auto-commit for d94b08b8-2d1a-4728-8aa3-7b92d578a874

--- components/CustomizationModal.jsx
+++ components/CustomizationModal.jsx
@@ -1803,25 +1803,6 @@ export default CustomizationModal
                       )}
                     </div>
 
-                    {/* Mobile Hat */}
-                    {playerData.equippedHat && (
-                      <div className={`absolute -top-3 left-1/2 transform -translate-x-1/2 ${
-                        playerData.equippedHat === 'crown_gold' ? 
-                          'w-8 h-5 bg-gradient-to-t from-yellow-500 to-yellow-300 rounded-t-lg border border-yellow-600' :
-                        playerData.equippedHat === 'cap_baseball' ? 
-                          'w-10 h-4 bg-gradient-to-r from-red-600 to-red-500 rounded-full' :
-                        playerData.equippedHat === 'helmet_space' ? 
-                          'w-12 h-7 bg-gradient-to-b from-gray-300 to-gray-500 rounded-full border-2 border-blue-400' :
-                          'w-6 h-4 bg-gray-600 rounded'
-                      }`}>
-                        {playerData.equippedHat === 'crown_gold' && (
-                          <div className="flex justify-center">
-                            <div className="w-1 h-2 bg-yellow-300 rounded-t-full mt-0.5"></div>
-                          </div>
-                        )}
-                      </div>
-                    )}
-
                     {/* Mobile Trail */}
                     {playerData.equippedTrail && (
                       <div className="absolute -right-14 top-1/2 transform -translate-y-1/2 flex space-x-1">
