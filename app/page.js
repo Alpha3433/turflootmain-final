850732921b850211d2dfc4f69943a3b67e04ed7c
Alpha3433 <badassborus@gmail.com>
2025-08-28 03:32:52 +0000

    auto-commit for 9b0a4f1a-783a-45e3-9086-0f82ebaeba10

--- app/page.js
+++ app/page.js
@@ -2163,7 +2163,7 @@ export default function Home() {
                   title="Party Lobby"
                 >
                   <div className="w-6 h-6 rounded-lg flex items-center justify-center border bg-orange-500/20 border-orange-500/30 group-hover:bg-orange-500/30 transition-all duration-200">
-                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="w-4 h-4 text-orange-400 group-hover:text-orange-300 transition-colors duration-200" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
                   </div>
