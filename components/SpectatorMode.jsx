a584cef54abb71158b39e40ca3f885948f6f54f6
Alpha3433 <badassborus@gmail.com>
2025-08-31 17:48:57 +0000

    auto-commit for ec9ee5db-3799-4d68-9a79-b56b48d44c51

--- components/SpectatorMode.jsx
+++ components/SpectatorMode.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
 import { useRouter } from 'next/navigation'
 import { usePrivy } from '@privy-io/react-auth'
 import { io } from 'socket.io-client'
