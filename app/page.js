c7e8b0a21bf66a14dde2b5946a50e6a494f61e6f
Alpha3433 <badassborus@gmail.com>
2025-09-05 05:59:37 +0000

    auto-commit for fd773b63-a1ce-4721-8483-4091c2fca34d

--- app/page.js
+++ app/page.js
@@ -899,7 +899,7 @@ export default function TurfLootTactical() {
         <div style={{
           position: 'absolute',
           left: '120px',
-          top: '80px',
+          top: '40px',
           width: '300px',  
           zIndex: 20,
           ...tacticalPanelStyle
