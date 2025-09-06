c257bfbf98907a54ce6629a6568af9616ca87755
Alpha3433 <badassborus@gmail.com>
2025-09-06 04:50:46 +0000

    auto-commit for bb2643d4-9506-451d-bc66-b391092d6c7a

--- app/page.js
+++ app/page.js
@@ -1856,8 +1856,8 @@ export default function TurfLootTactical() {
               </div>
               <input 
                 type="text" 
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
+                value={customUsername || userName}
+                onChange={(e) => setCustomUsername(e.target.value)}
                 style={nameInputStyle}
                 placeholder="USERNAME"
               />
