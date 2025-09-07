96b207dd4431fc85a1945323913c393a152535f0
Alpha3433 <badassborus@gmail.com>
2025-09-07 15:08:52 +0000

    auto-commit for 5c783ede-78df-45ae-bdde-86a10287cfad

--- app/agario/page.js
+++ app/agario/page.js
@@ -187,7 +187,7 @@ const AgarIOGame = () => {
       this.updateCamera()
       
       // Maintain coin count
-      while (this.coins.length < 100) {
+      while (this.coins.length < 250) { // Increased from 100 to 250 to match generateCoins()
         const centerX = this.world.width / 2  // 2000
         const centerY = this.world.height / 2 // 2000
         const playableRadius = 1800 // Same as boundary radius
