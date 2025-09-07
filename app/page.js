7ed88a290a9d4d472c9346cee1d73a78b1748458
Alpha3433 <badassborus@gmail.com>
2025-09-07 16:19:19 +0000

    auto-commit for 86a6873c-7507-46a1-a6ee-ae7db1e9d9ae

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import ServerBrowserModal from '@/components/ServerBrowserModal'
 
