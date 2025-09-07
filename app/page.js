fb1aece16ecf1a2c8c9f10641820dd8cc9287d28
Alpha3433 <badassborus@gmail.com>
2025-09-07 16:52:05 +0000

    auto-commit for 63dbdb8d-2c51-42a3-b4cd-810c20e9019b

--- app/page.js
+++ app/page.js
@@ -3871,7 +3871,7 @@ export default function TurfLootTactical() {
           </div>
           
           <button 
-            onClick={() => createSkinStorePopup(currency)}
+            onClick={() => createSkinStorePopup(currency, setCurrency)}
             style={{
             width: '100%',
             padding: '14px',
