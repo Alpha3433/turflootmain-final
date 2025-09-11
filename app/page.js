ca457db0ca3d3e8ce9a65d3700f828b3ceadcd58
Alpha3433 <badassborus@gmail.com>
2025-09-11 08:18:24 +0000

    auto-commit for ea2696a3-22b8-4828-8f70-7b9339957292

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import { useState, useEffect, useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import { usePrivy, useWallets } from '@privy-io/react-auth'
-import { useFundWallet } from '@privy-io/react-auth/solana'
+// useFundWallet will be loaded dynamically to avoid SSR issues
 import ServerBrowserModal from '@/components/ServerBrowserModal'
 
 export default function TurfLootTactical() {
