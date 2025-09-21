2b02635bda912a5490b879dde8b267d36b4fdbc2
Alpha3433 <badassborus@gmail.com>
2025-09-21 13:34:13 +0000

    auto-commit for f659c95f-d822-4165-9e8f-1bd8c0691d7f

--- frontend/.next/standalone/.next/server/server-reference-manifest.js
+++ frontend/.next/standalone/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"2aDkRwRXnWvhNaeobGIbP0Udr2C041rnR4WZuJYWZr0=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"oytCEId9fUR8cZCb+Ke5F8ta/W/VUXeD25lldTKdXIg=\"}"
