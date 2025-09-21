acbcab26af74f58303b49dcd02f32d962d0033a4
Alpha3433 <badassborus@gmail.com>
2025-09-21 05:59:07 +0000

    auto-commit for aeb38c3f-30ac-406c-b182-1fe14cebabe2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,6 @@ module.exports = {
       script: "build/index.js",
       instances: 1,
       exec_mode: "fork",
-      // No external env file references - use .env.production auto-loaded by @colyseus/tools
       env: {
         NODE_ENV: "production"
       }
