2e8c0acb38949234c16a45c0191fd19d6d9925ec
Alpha3433 <badassborus@gmail.com>
2025-09-21 07:56:42 +0000

    auto-commit for 30ccccd0-6ec5-402c-891a-1fc9594a1748

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -5,7 +5,12 @@ module.exports = {
       script: "build/index.js",
       instances: 1,
       exec_mode: "fork",
+      node_args: "--max-old-space-size=2048",
       env: {
+        NODE_ENV: "production",
+        PORT: "2567"
+      },
+      env_production: {
         NODE_ENV: "production",
         PORT: "2567"
       }
