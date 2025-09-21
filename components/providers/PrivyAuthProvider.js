ebb8c6d388154405dab5cd06f48bce359c5c024f
Alpha3433 <badassborus@gmail.com>
2025-09-21 06:12:07 +0000

    auto-commit for ed650541-72e8-485e-bde0-6cb2aa923162

--- components/providers/PrivyAuthProvider.js
+++ components/providers/PrivyAuthProvider.js
@@ -4,7 +4,6 @@ import { PrivyProvider } from '@privy-io/react-auth'
 import { Component, useState, useEffect } from 'react'
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
 import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
-import { BackpackWalletAdapter } from '@solana/wallet-adapter-backpack'
 
 // Error boundary for Privy-related errors
 class PrivyErrorBoundary extends Component {
